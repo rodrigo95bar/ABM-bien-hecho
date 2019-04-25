9e08339c0114a38a239b79f2a8dc530c1c4205c0
rodrigo95bar <47338073+rodrigo95bar@users.noreply.github.com>
2019-04-25 18:48:01 -0300

    Add files via upload

---
+++ ABM-PROGRESO-master/Clase7_completado/Pantallas.h
@@ -0,0 +1,26 @@
+#ifndef PANTALLAS_H_INCLUDED
+#define PANTALLAS_H_INCLUDED
+typedef struct
+{
+    char nombre[50];
+    char direccion[250];
+    float precio;
+    int tipo;
+    int isEmpty; // lleno=0 ; vacio=1
+    int idPantalla; //autoincremental
+} Pantalla;
+
+int pan_initializeArray(Pantalla* pPantallas, int len);
+
+int pan_showArray(Pantalla* pPantallas,int len);
+
+int pan_create(Pantalla* pPantallas,int len, int pIndex,int idPan, char* msgE);
+
+int pan_searchFreeSpace(Pantalla* pPantallas, int len);
+
+int pan_findPosID(Pantalla* pPantallas, int len,int idPan);
+
+int pan_bajaLogica(Pantalla* pPantallas, int len,char* msgE);
+
+
+#endif // PANTALLAS_H_INCLUDED
